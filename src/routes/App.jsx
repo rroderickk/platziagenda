76483c3840c95d7b2cc0061399e5df92079bb972
rroderickk <rroderickk@gmail.com>
2022-03-21 14:25:27 -0500

    [ADD]

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -10,44 +10,6 @@ const App =()=> { return (
 	<Layout>
 		<Routes>
 			<Route exact path="/" 
-				element={ <PlatziAgenda date="" highTitle="" who="" hour=""/>} />
-
-			<Route exact path="/viernes13" 
-				element={
-				 <PlatziAgenda
-					date="viernes 13"
-					highTitle="como hackear visual estudio code con vim"
-					who="cheatmodes4"
-					hour="4:00 am hora bogota"
-				 />} />
-
-			<Route exact path="/viernes14" 
-				element={
-					 <PlatziAgenda
-						date="viernes 14"
-						hightTitle="Blogueando desde el router ReactJs"
-						who="cheatModes4"
-						hour={<em>4:00 am hora bogota</em>}
-					>
-						<Children 
-							hightTitle={<><em>es hora</em> "de usar mas las HOCS"</>}
-							who="juanDc es batman"
-							hour={<s>8:00 am hora bogota</s>}
-						/>
-						<Children 
-							hightTitle="como hackear servidores de la nasa"
-							who="oscar barajas"
-							hour={<b>9:00 am hora mexico</b>}
-						/>
-						<Children 
-							hightTitle="HOC deberia conectarse a la api"
-							who="platzi agenda y dar info en tiempo real"
-							hour="9:00 am hora mexico"
-						/>
-				 	</PlatziAgenda>}
-			/>
-
-			<Route exact path="/domingo20" 
 				element={
 					 <PlatziAgenda
 						date="domingo 20"
@@ -73,6 +35,43 @@ const App =()=> { return (
 				 	</PlatziAgenda>}
 			/>
 
+			<Route path="/ejemplo" 
+				element={ <PlatziAgenda date="" highTitle="" who="" hour=""/>} />
+
+			<Route exact path="/viernes13" 
+				element={
+				 <PlatziAgenda
+					date="viernes 13"
+					highTitle="como hackear visual estudio code con vim"
+					who="cheatmodes4"
+					hour="4:00 am hora bogota"
+				 />} />
+
+			<Route path="/viernes14" 
+				element={
+					 <PlatziAgenda
+						date="viernes 14"
+						hightTitle="Blogueando desde el router ReactJs"
+						who="cheatModes4"
+						hour={<em>4:00 am hora bogota</em>}
+					>
+						<Children 
+							hightTitle={<><em>es hora</em> "de usar mas las HOCS"</>}
+							who="juanDc es batman"
+							hour={<s>8:00 am hora bogota</s>}
+						/>
+						<Children 
+							hightTitle="como hackear servidores de la nasa"
+							who="oscar barajas"
+							hour={<b>9:00 am hora mexico</b>}
+						/>
+						<Children 
+							hightTitle="HOC deberia conectarse a la api"
+							who="platzi agenda y dar info en tiempo real"
+							hour="9:00 am hora mexico"
+						/>
+				 	</PlatziAgenda>}
+			/>
 		</Routes>
 	</Layout>
 </BrowserRouter>
